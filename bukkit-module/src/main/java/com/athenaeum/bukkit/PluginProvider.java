ab7e24a95a7620276dfad18ec83499d90c35f003
Ilya Koreisha <koreisha2000@mail.ru>
2020-12-24 13:14:25 +0300

    Updated plugin provider. Fixed? (Completely ye but here we use this as Library folder. It's mean, here lib can load dependencies or smth similar. Config files also included) Pull: https://github.com/MaxWainer/athenaeum/pull/2

--- bukkit-module/src/main/java/com/athenaeum/bukkit/PluginProvider.java
+++ bukkit-module/src/main/java/com/athenaeum/bukkit/PluginProvider.java
@@ -2,6 +2,7 @@ package com.athenaeum.bukkit;
 
 import com.athenaeum.bukkit.plugin.AthenaeumPlugin;
 import lombok.Getter;
+import org.bukkit.plugin.PluginDescriptionFile;
 
 import java.io.File;
 
@@ -16,8 +17,29 @@ public class PluginProvider {
         providing = plugin;
     }
 
+    // Max_Wainer start
+
     public File getLibraryFolder() {
-        return new File("plugins/Athenaeum");
+        return new File("athenaeumAddons/" + providing.getName());
     }
+
+    public void loadLibraryFolder() {
+        if(!getLibraryFolder().exists())
+            getLibraryFolder().mkdir();
+    }
+
+    public File getProvidingFolder() {
+        return getProviding().getDataFolder();
+    }
+
+    public String getProvidingName() {
+        return getProviding().getName();
+    }
+
+    public PluginDescriptionFile getProvidingDescription() {
+        return getProviding().getDescription();
+    }
+
+    // Max_Wainer end
 
 }
